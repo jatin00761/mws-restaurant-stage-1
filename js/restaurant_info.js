3e28e2267b9687741473ca11bf4096b76b0ea41b
Jatin Pandey <jatinpandey77@gmail.com>
2018-06-24 20:25:28 +0530

    Added Mapbox API Key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamF0aW5wYW5kZXkiLCJhIjoiY2ppc3h5aDduMGZsajNwbGd5MGhscGxwNyJ9.zO-GMJ6zAkDr50YeYOYCGg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
