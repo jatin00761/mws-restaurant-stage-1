1db7466fb9ebfdfa7acea4cda58030db23e9638b
Jatin Pandey <jatinpandey77@gmail.com>
2018-07-01 19:07:46 +0530

    Cache remove added. Contrast imroved, reviews list updated.

--- register-sw.js
+++ register-sw.js
@@ -1,5 +1,8 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load',function(){
-        navigator.serviceWorker.register('sw.js')
-    })
-}
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js')
+    .then(registration => {
+        console.log(`Registered!! || Scope:  ${registration.scope}`);
+    }).catch(error => {
+        console.log(`Failed: , error: ${error}`);
+    });
+} 
--- sw.js
+++ sw.js
@@ -1,30 +1,65 @@
-let CacheName = 'restaurant-review-1';
-const urlsToCache = [
-  '/index.html',
-  '/register-sw.js',
-  '/restaurant.html',
-  '/js',
-  '/img',
-  '/data',
-];
+const staticCacheName = 'restaurant-stage-1';
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CacheName)
-        .then(function(cache){
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-self.addEventListener('activate',function(event){
-  console.log('Activated!!');
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll([
+          '/index.html',
+          '/css/styles.css',
+          '/js/dbhelper.js',
+          '/js/register-sw.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          '/data/restaurants.json',
+          '/restaurant.html?id=1',
+          '/restaurant.html?id=2',
+          '/restaurant.html?id=3',
+          '/restaurant.html?id=4',
+          '/restaurant.html?id=5',
+          '/restaurant.html?id=6',
+          '/restaurant.html?id=7',
+          '/restaurant.html?id=8',
+          '/restaurant.html?id=9',
+          '/restaurant.html?id=10'
+        ]).catch(error => {
+          console.log('Caches open failed: ' + error);
+        });
       })
-    );
-  });
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(staticCacheName).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(error => {
+      if (event.request.url.includes('.jpg')) {
+        return caches.match('/img/fixed/offline_img1.png');
+      }
+      return new Response('No Internet', {
+        status: 404,
+        statusText: "No internet"
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('restaurant-stage-') && cacheName !== staticCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
